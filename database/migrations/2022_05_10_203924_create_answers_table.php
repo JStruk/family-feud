c4f840d86ca5006b8b3f9b4b0d8a289c5c231a04
Vehikl <vehikl@Jacare.local>
2022-05-10 16:44:00 -0400

    create Question and Answer models & migrations

---
+++ database/migrations/2022_05_10_203924_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->unsignedBigInteger('question_id');
+            $table->unsignedInteger('weight');
+            $table->foreign('question_id')->references('id')->on('questions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
