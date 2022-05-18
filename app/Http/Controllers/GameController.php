ef3c1cba3b4c16f9189e403492b249323e5317e3
JStruk <strukjustin@gmail.com>
2022-05-17 20:58:05 -0400

    create and return a new game with questions and answers on POST /game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     public function create(Request $request)
     {
-        return "creating game";
+        $questions = Question::query()->inRandomOrder()->limit(5)->with('answers')->get();
+        $game = Game::query()->create()->questions()->attach($questions);
+
+        return [
+            "game" => $game,
+            "questions" => $questions
+        ];
     }
 }
